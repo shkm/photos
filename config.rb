b12776a6f5bd2449c4ac4802f7a57038193e2165
Jamie Schembri <jamie@lico.nl>
2017-08-19 16:13:44 +0200

    Add cname after build

--- config.rb
+++ config.rb
@@ -47,6 +47,9 @@ helpers AlbumHelpers
 # Build
 
 set :build_dir, 'docs'
+after_build do |builder|
+  File.open("#{Dir.pwd}/#{config.build_dir}/CNAME", 'w') { |f| f.write 'photos.schembri.me' }
+end
 # configure :build do
 #   activate :minify_css
 #   activate :minify_javascript
