ca9fb443d5ac47b9fdb898fb9d46bb6066980228
Jamie Schembri <jamie@lico.nl>
2017-08-19 22:25:42 +0200

    Do not require date or cover photo

--- models/album.rb
+++ models/album.rb
@@ -2,9 +2,9 @@ class Album < ApplicationRecord
   has_many :photos
   belongs_to :cover_photo, class_name: 'Photo'
 
-  validates :name, :cover_photo, presence: true
+  validates :name, presence: true
 
   def human_date
-    date.strftime("%e %B ’%y")
+    date&.strftime("%e %B ’%y")
   end
 end
